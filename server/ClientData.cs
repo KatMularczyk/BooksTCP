e4426ab66fcae5b2717c3b366ac604c73237207a
KatMularczyk <k.mularczyk22@gmail.com>
2024-01-17 21:49:20 +0100

    gitignored changed

---
+++ server/ClientData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksTCP
+{
+    internal class ClientData
+    {
+        public Socket clientSocket;
+        public Thread clientThread;
+        public string id;
+
+        public ClientData()
+        {
+            id = Guid.NewGuid().ToString();
+        }
+
+        public ClientData(Socket clientSocket)
+        {
+            id = Guid.NewGuid().ToString();
+            this.clientSocket = clientSocket;
+        }
+
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Sockets;
+using System.IO;
+using ServerData;
+using System.Net;
+
+namespace BooksTCP
+{
+    internal class Program
+    {
+        static Socket listenerSocket;
+        static List<ClientData> _clients;
+        static void Main(string[] args)
+        {
+            listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); //create socket
+            _clients = new List<ClientData>(); //create the clients list
+
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse(Packet.GetIP4Address()), 4242); //create IP endpoint with our ip
+
+            listenerSocket.Bind(ip); //bind the local ip to our socket
+
+            Thread listenThread = new Thread(ListenThread);
+            listenThread.Start();
+
+        }
+
+        static void ListenThread()
+        {
+            for (; ; )
+            {
+                listenerSocket.Listen();
+                _clients.Add(new ClientData(listenerSocket.Accept()));
+            }
+
+        }
+
+    }
+}
